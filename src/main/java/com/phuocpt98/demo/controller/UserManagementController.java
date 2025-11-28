cdef13d63b62ccf50432c2388d3548a1984530c8
Phan Tiến Phước <phantienphuoc@Mac-mini-cua-Phan.local>
2025-11-28 16:54:51 +0700

    build: commit 1

---
+++ src/main/java/com/phuocpt98/demo/controller/UserManagementController.java
@@ -0,0 +1,12 @@
+package com.phuocpt98.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserManagementController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello ba";
+    }
+}
